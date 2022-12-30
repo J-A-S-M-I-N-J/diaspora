035431c48e5fb3daca7f99aabc68523a4525b75c
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2022-12-30 01:27:21 +0100

    made the base for the home page and added font

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -144,6 +144,12 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
