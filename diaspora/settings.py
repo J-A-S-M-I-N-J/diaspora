23eb1da2b3494f0f949ec449303c557b01958fd3
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2023-01-02 23:13:46 +0100

    error in grand total fixed in apps.py and checkout signals

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -169,7 +169,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-#STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
