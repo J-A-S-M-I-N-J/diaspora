9f48e5a0e6ae9b33a9945467275536c9328b7674
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2023-01-19 14:08:25 +0100

    added home button to mobile view since logo is hidden

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, Inventory
 
 # Register your models here.
 
@@ -20,5 +20,14 @@ class CategoryAdmin(admin.ModelAdmin):
         'friendly_name',
         'name',
     )
+
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'product',
+        'quantity',
+    )
+
+
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Inventory, InventoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Category(models.Model):
     """A model to create a category for the products"""
@@ -24,6 +26,27 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
+    
     def __str__(self):
         return self.name
+
+    def get_inventory(self):
+        return self.inventory.quantity
+
+class Inventory(models.Model):
+    """A model to create a product inventory"""
+    product = models.ForeignKey('Product', null=True, blank=True, on_delete=models.SET_NULL)
+    quantity = models.IntegerField(null=True, blank=True, default=0)
+
+    def __str__(self):
+        return self.product.name
+
+@receiver(post_save, sender=Product)
+def create_or_update_inventory(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        Inventory.objects.create(product=instance)
+    # Existing users: just save the profile
+    instance.Inventory.save()
