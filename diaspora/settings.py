700645d91af95b637a73ba798ca3734b8ce600fb
Jasmin Juzbasic <jasmin.juzbasic@gmail.com>
2023-01-19 12:21:07 +0100

    mailchimp not rendering 403 error, trying again

--- diaspora/settings.py
+++ diaspora/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['diaspora-website.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['diaspora-website.herokuapp.com', 'localhost', '127.0.0.1', '127.0.0.1:8000']
 
 EMAIL_HOST = 'smtp.mailtrap.io'
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
